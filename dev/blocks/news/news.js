d599ab662e73c4ec26c23f8c9ba9d4bb4e5aac13
Igor Adamenko <mail@igoradamenko.com>
2015-12-07 00:19:24 +0300

    fix disqus count.js creating

--- dev/blocks/news/news.js
+++ dev/blocks/news/news.js
@@ -132,6 +132,7 @@ $(function() {
 				var s = document.createElement('script');
 				s.async = true;
 				s.type = 'text/javascript';
+				s.id = 'dsq-count-scr';
 				s.src = '//' + disqusID + '.disqus.com/count.js';
 				
 				(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
