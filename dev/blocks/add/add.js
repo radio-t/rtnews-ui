d14501e87f03b71b5a0b9a3ff9e4e437ca380f8e
Igor Adamenko <mail@igoradamenko.com>
2015-12-13 02:36:19 +0300

    add test version of new quick-add

--- dev/blocks/add/add.js
+++ dev/blocks/add/add.js
@@ -34,44 +34,72 @@ $(function() {
 	if ($('#add__bookmark').length) {
 		$('#add__bookmark')
 			.attr('href', function() {
-				var hrefFunc = function() {
-					var w = window,
-						d = document,
-						i = d.createElement('iframe'),
-						s = i.style;
+				// var hrefFunc = function() {
+				// 	var w = window,
+				// 		d = document,
+				// 		i = d.createElement('iframe'),
+				// 		s = i.style;
 
-					i.src = '#LOCATION#'
-							+ '?url=' + encodeURIComponent(location.href)
-							+ '&login=' + '#LOGIN#'
-							+ '&password=' + '#PASSWORD#';
-					i.scrolling = 'no';
+				// 	i.src = '#LOCATION#'
+				// 			+ '?url=' + encodeURIComponent(location.href)
+				// 			+ '&login=' + '#LOGIN#'
+				// 			+ '&password=' + '#PASSWORD#';
+				// 	i.scrolling = 'no';
 
-					s.transition = 'all 200ms';
-					s.position = 'fixed';
-					s.left = '0';
-					s.right = '0';
-					s.top = '0';
-					s.zIndex = 16777271;
-					s.height = '70px';
-					s.width = '100%';
-					s.overflow = 'hidden';
-					s.background = '#fff';
-					s.borderBottom = '1px solid #bdbdbd';
-					s.boxShadow = '0 0 30px #828282';
+				// 	s.transition = 'all 200ms';
+				// 	s.position = 'fixed';
+				// 	s.left = '0';
+				// 	s.right = '0';
+				// 	s.top = '0';
+				// 	s.zIndex = 16777271;
+				// 	s.height = '70px';
+				// 	s.width = '100%';
+				// 	s.overflow = 'hidden';
+				// 	s.background = '#fff';
+				// 	s.borderBottom = '1px solid #bdbdbd';
+				// 	s.boxShadow = '0 0 30px #828282';
 
-					d.body.appendChild(i);
+				// 	d.body.appendChild(i);
 					
-					w.setTimeout(function() {
-						s.height = '0';
+				// 	w.setTimeout(function() {
+				// 		s.height = '0';
 
-						w.setTimeout(function() {
-							i.parentNode.removeChild(i);
-						}, 300);
-					}, 3000);
+				// 		w.setTimeout(function() {
+				// 			i.parentNode.removeChild(i);
+				// 		}, 300);
+				// 	}, 3000);
+				// }
+
+				var hrefFunc = function() {
+					var regex = new RegExp("[\\?&]url=([^&#]*)"),
+					    results = regex.exec(location.search);
+
+					url = results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+
+					var request = new XMLHttpRequest();  
+
+					request.open('POST', '#LOCATION#' + '/news', false, '#LOGIN#', '#PASSWORD#'); 
+					request.setRequestHeader('Authorization', 'Basic ' + btoa('#LOGIN#' + ':' + '#PASSWORD#')); 
+
+					try {
+						request.send('{ "link": "' + decodeURIComponent(location.href) + '" }');
+
+				        if (request.readyState === 4) { 
+				        	if (request.status !== 200) {
+				        		alert('Не смог сохранить');
+				        	}  else {
+				        		alert('Сохранил');
+				        	}
+				        } else {
+				        	alert('Не смог достучаться до сервера');
+				        }
+					} catch (err) {
+						alert('Не смог сохранить');
+					}
 				}
 
 				var href = hrefFunc.toString()
-								   .replace(/#LOCATION#/g, location.protocol + '//' + location.host + '/qadd/')
+								   .replace(/#LOCATION#/g, APIPath.indexOf('http') == 0 ? APIPath : location.protocol + '//' + location.host + APIPath)
 								   .replace(/#LOGIN#/g, login)
 								   .replace(/#PASSWORD#/g, password);
 
