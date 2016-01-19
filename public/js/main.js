e6e15d38b9c6ccc4088ad6b7cf7353fd272097ab
Igor Adamenko <mail@igoradamenko.com>
2016-01-19 21:16:32 +0300

    add show start link

--- public/js/main.js
+++ public/js/main.js
@@ -294,6 +294,23 @@ $(function() {
 		$('.menu__item_admin').css('display', 'inline-block');
 		$('.menu__item_user').hide();
 	}
+
+	$('#start').click(function(event) {
+		event.preventDefault();
+
+		$.ajax({
+			url: APIPath + '/show/start',
+			type: 'PUT',
+			headers: authHeaders
+		})
+		.done(function(json) {
+			notify('Поехали!', null, 1500);
+		})
+		.fail(function(response) {
+			console.log("error while starting show");
+			console.log(response);
+		});
+	});
 });
 $(function() {
 	var $login = $('#login');
