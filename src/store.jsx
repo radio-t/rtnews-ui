61a663a6cb0f4801baa60cfa1f4bd01bbabf8e49
Vyrtsev Mikhail <reeywhaar@gmail.com>
2018-12-03 22:56:17 +0300

    fix store issue -> issueNumber

--- src/store.jsx
+++ src/store.jsx
@@ -3,7 +3,7 @@ import { createElement } from "react";
 import { createStore } from "redux";
 
 const initialState = {
-	issue: null,
+	issueNumber: null,
 	isAdmin: false,
 	notifications: [],
 	activeId: null,
